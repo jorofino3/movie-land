f9fd96d39c4a5e45a48f8d2a234b8a588a7fde0b
joeyorofino <97992795+jorofino3@users.noreply.github.com>
2022-08-06 13:29:39 -0400

    fixed file structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,14 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+
+import "./App.css";
 
 function App() {
   const API_URL = "http://www.omdbapi.com?apikey=a55351fd";
 
-  return (
-    <div className="App">
-      
-    </div>
-  );
+  //call to the API to search a movie
+  const searchMovies = async (title) => {
+    //retrieval
+    const response = await fetch(`${API_URL}&s=${title}`);
+    //data returned from the API call
+    const data = await response.json();
+
+    console.log(data);
+  };
+
+  useEffect(() => {
+    searchMovies("Harry Potter");
+  }, []);
+  return <div>App</div>;
 }
 
 export default App;
