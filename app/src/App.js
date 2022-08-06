9f93a1ffa16bf356c75724e3b8d244dc71584907
joeyorofino <97992795+jorofino3@users.noreply.github.com>
2022-08-06 13:43:25 -0400

    added MovieCard and functions

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-
+import SearchIcon from "./images/search.svg";
 import "./App.css";
 
 function App() {
@@ -18,7 +18,29 @@ function App() {
   useEffect(() => {
     searchMovies("Harry Potter");
   }, []);
-  return <div>App</div>;
+  return (
+    <div className='app'>
+      <h1>MovieLand</h1>
+
+      <div className='search'>
+        <input
+          placeholder='Search for movies'
+          value='Harry Potter'
+          onChange={() => {
+            //change to come
+          }}
+        />
+
+        <img
+          src={SearchIcon}
+          alt='search'
+          onClick={() => {
+            //when the user clicks the search
+          }}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default App;
