9be8ba7b127f541f5eb09c56a5320842bfec5907
joeyorofino <97992795+jorofino3@users.noreply.github.com>
2022-08-06 13:18:40 -0400

    api integeration

--- app/src/App.js
+++ app/src/App.js
@@ -2,22 +2,11 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const API_URL = "http://www.omdbapi.com?apikey=a55351fd";
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
     </div>
   );
 }
